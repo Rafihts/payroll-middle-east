2488de2bd3242d8f98c8e6a771898ed6280cf5b5
hiibanashid15 <hiba@claudion.com>
2022-09-22 07:10:20 +0000

    log file added,patches folder moved

--- payroll_middle_east/patches/create_middle_east_custom_field.py
+++ payroll_middle_east/patches/create_middle_east_custom_field.py
@@ -2,8 +2,9 @@ from payroll_middle_east.customisation.salary import make_employee_custom_fields
 import frappe
 
 
-
 def execute():
+    logger = frappe.logger("patch_log", allow_site=True, file_count=50)
+    logger.info(f"patches has started to execute")
     regions = ['Saudi Arabia', 'UAE', 'Qatar', 'Oman', 'Kuwait', 'Bahrain']
     companies = frappe.get_all('Company',
                                filters={'country':
@@ -12,14 +13,14 @@ def execute():
                                pluck='company_name'
                                )
     if not companies:
+        logger.info('no companies found,returning')
         return
-
+    logger.info('companies found!')
     employess = frappe.get_all(
         'Employee', filters={'company': ['in', companies]})
 
     if not employess:
+        logger.info('Employees not found,returning')
         return
-
+    logger.info('Making custom field for employee DocType')
     make_employee_custom_fields()
-
-
